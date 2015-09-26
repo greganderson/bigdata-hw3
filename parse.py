12262d224f5314e5f64e93b41e8ebdfaf07d0e05
Greg Anderson <greg@infobytes.com>
2015-09-26 05:22:10 -0600

    Added WikiExtractor and the ability to import it.

--- parse.py
+++ parse.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
+import wikiextractor.WikiExtractor
 
 f = 'small_pages/page-0001000.xml'
-tree = ET.parse(f)
-root = tree.getroot()
-text = root[3][7]
 
 # Turn every abnormal character into a space?

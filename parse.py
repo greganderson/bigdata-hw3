ff62f8e96804b32262fd2daf711e0b078d5c2d41
Greg Anderson <greg@infobytes.com>
2015-09-30 08:35:27 -0600

    More test code for parse.

--- parse.py
+++ parse.py
@@ -1,5 +1,10 @@
-import wikiextractor.WikiExtractor
+import sys, subprocess
 
-f = 'small_pages/page-0001000.xml'
+fname = 'small_pages/page-0001000.xml'
+
+proc = subprocess.call([sys.executable, 'wikiextractor/WikiExtractor.py', fname, '-l','-a'], stdout=subprocess.PIPE)
+output = proc.communicate()
+
+
 
 # Turn every abnormal character into a space?

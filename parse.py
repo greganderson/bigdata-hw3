60811b0c6a8ef14a0cba2eb9cfeb25f438b94d34
Greg Anderson <greg@infobytes.com>
2015-10-01 09:37:59 -0600

    Closed #4

--- parse.py
+++ parse.py
@@ -1,10 +1,12 @@
 #from pyspark import SparkContext, SparkConf
 import wikiextractor.WikiExtractor as wikix
-import sys, os
+import sys, os, re
 from contextlib import contextmanager
 
 
 ### CONFIGURATION ###
+
+
 
 """
 conf = SparkConf()
@@ -25,6 +27,18 @@ def read_files(f):
 
 f = 'small_pages/page-0001000.xml'
 s = wikix.main(['-l', '-a', f])
+s = s.replace('%20', ' ')
+
+p = re.compile('<a href=".+?".*?>')
+a = p.findall(text)
+
+# Convert anchor tags to just the link
+b = map(lambda x: x[9:x.rfind('"')], a)
+
+
+with open('file.txt', 'w') as new_f:
+	new_f.write(s.encode('utf8'))
+
 #files = sc.wholeTextFiles('small_pages/*')
 #converted = files.map(read_files)
 #converted.first()

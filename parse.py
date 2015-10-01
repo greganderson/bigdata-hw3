f86a332bf3536c1a69c84f695f7a026d9f282478
Jesus <chuy8jay@gmail.com>
2015-10-01 16:00:17 -0600

    Added word count

--- parse.py
+++ parse.py
@@ -1,5 +1,21 @@
-import wikiextractor.WikiExtractor
+from pyspark import SparkContext, SparkConf
 
-f = 'small_pages/page-0001000.xml'
+### CONFIGURATION ###                                                                                                          
+conf = SparkConf()
+conf.setMaster("local[4]")
+conf.setAppName("reduce")
+conf.set("spark.executor.memory", "4g")
 
-# Turn every abnormal character into a space?
+sc = SparkContext(conf=conf)
+
+lines = sc.textFile("textFile.txt")
+
+l = lines.flatMap(lambda line: line.split(" ")) \
+     .filter(lambda w: len(w) > 0) \
+     .map(lambda word: (word, 1)) \
+     .reduceByKey(lambda x,y: x+y) \
+     .sortBy(lambda x: x[1], False)
+
+l = l.collect()
+with open('result.txt', 'w') as fl:
+    fl.write(str(l))

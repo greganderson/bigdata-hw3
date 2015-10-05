6cb6abe27c4e09103898793e45e4055d4fa172cd
Greg Anderson <greg@infobytes.com>
2015-10-05 10:18:29 -0600

    Connected web service with spark. Close #28

---
+++ server.py
@@ -0,0 +1,41 @@
+import time, sys, cherrypy, os
+import translogger
+from app import create_app
+from pyspark import SparkContext, SparkConf
+
+def init_spark_context():
+	# load spark context
+	conf = SparkConf().setAppName("WikiSearcher")
+	# IMPORTANT: pass aditional Python modules to each worker
+	sc = SparkContext(conf=conf, pyFiles=['parse.py', 'app.py'])
+
+	return sc
+
+
+def run_server(app):
+
+	# Enable WSGI access logging via Paste
+	app_logged = translogger.TransLogger(app)
+
+	# Mount the WSGI callable object (app) on the root directory
+	cherrypy.tree.graft(app_logged, '/')
+
+	# Set the configuration of the web server
+	cherrypy.config.update({
+		'engine.autoreload.on': True,
+		'server.socket_port': 5433,
+		'server.socket_host': '127.0.0.1'
+	})
+
+	# Start the CherryPy WSGI web server
+	cherrypy.engine.start()
+	cherrypy.engine.block()
+
+
+if __name__ == "__main__":
+	# Init spark context and load libraries
+	sc = init_spark_context()
+	app = create_app(sc)
+
+	# start web server
+	run_server(app)
